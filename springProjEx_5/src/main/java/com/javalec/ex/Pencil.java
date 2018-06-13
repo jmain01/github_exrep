e9d021df226a35bcb73cb98287da9833e3afbc1b
JM <JM@DESKTOP-HD0VFAJ>
2018-06-14 01:06:26 +0900

    first_commit

---
+++ springProjEx_5/src/main/java/com/javalec/ex/Pencil.java
@@ -0,0 +1,8 @@
+package com.javalec.ex;
+
+public interface Pencil {
+
+ public	void use();
+ 
+}
+
---
+++ springProjEx_5/src/main/java/com/javalec/ex/Pencil6BWithEraser.java
@@ -0,0 +1,11 @@
+package com.javalec.ex;
+
+public class Pencil6BWithEraser implements Pencil {
+
+	@Override
+	public void use() {
+
+		System.out.println("6B입니다. 지우개가 있습니다.");
+	}
+
+}
